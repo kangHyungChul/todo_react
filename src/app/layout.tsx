e1875a3218f942af1d1317ab782ebf7e9446e973
hyungchul.kang <hyungchul.kang@concentrix.com>
2025-05-16 18:00:57 +0900

    setting중...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import { Nanum_Gothic } from 'next/font/google';
 import './globals.scss';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
 
 const nanumGothic = Nanum_Gothic({
     subsets: ['latin'],
